d7cf0e4b7eae7f5d1fe4b7d08d7a3e2d526f1858
Carlos Cueto <a01209474@itesm.mx>
2018-10-10 18:35:12 -0500

    auto render and parameter of random sample

--- main.py
+++ main.py
@@ -11,18 +11,44 @@ import numpy as np
 
 import urllib
 import random
+import sys
+import argparse
+import math
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--samples", type=int, help="Choose how many samples to retrive for train and test even number")
+args = parser.parse_args()
+
+rand_samples = 4
+
+if args.samples:
+  rand_samples = args.samples
+  print("samples on")
+
+
 
 # Download database from MNIST to train the model
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
 
 #Get random samples from database to train
-random_X_train = X_train[np.random.choice(X_train.shape[0], 4, replace=False), :]
+random_X_train = X_train[np.random.choice(X_train.shape[0], rand_samples, replace=False), :]
 
 #Get random samples from database to test
-random_X_test = X_test[np.random.choice(X_test.shape[0], 4, replace=False), :]
+random_X_test = X_test[np.random.choice(X_test.shape[0], rand_samples, replace=False), :]
 
+print(math.ceil(rand_samples / 2), ',' , 2)
+fig=plt.figure(figsize=(8, 6))
 
+columns = 2
+rows = math.ceil(rand_samples / 2)
+aux = 220
+for i in range(0, columns * rows):
+  fig.add_subplot(rows, columns, i + 1, )
+  #plt.subplot(aux + 1)
+  plt.imshow(random_X_train[i], cmap=plt.get_cmap('gray'))
+  print(i)
+"""
 plt.subplot(221)
 plt.imshow(random_X_train[0], cmap=plt.get_cmap('gray'))
 plt.subplot(222)
@@ -31,6 +57,6 @@ plt.subplot(223)
 plt.imshow(random_X_train[2], cmap=plt.get_cmap('gray'))
 plt.subplot(224)
 plt.imshow(random_X_train[3], cmap=plt.get_cmap('gray'))
-
+"""
 # show the plot
 plt.show()
